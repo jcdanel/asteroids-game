802945cda9b1b39b1c763d3fdaae485cc286b41c
jcdanel <jakechrisdanel@gmail.com>
2025-01-19 13:11:37 -0700

    v0.4, added asteroids and a field for them to move around in

---
+++ asteroid.py
@@ -0,0 +1,14 @@
+import pygame
+from circleshape import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+def draw(self, screen):
+        pygame.draw.circle(screen, "white", (self.x, self.y), self.radius, 2)
+
+def update(self, dt):
+    self.position += CircleShape.velocity * dt
--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -12,23 +14,28 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield_obj = AsteroidField()
     
     while True:
-        dt = fps.tick(60)/1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
 
         for update in updatable:
-            updatable.update(dt)
-
+            update.update(dt)
         for draw in drawable:
-            drawable.draw(screen)
+            draw.draw(screen)
+
         pygame.display.flip()
         fps.tick(60)
+        dt = fps.tick(60)/1000
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
